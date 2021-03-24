cf89c7220a9c67be8243ec2568a8ff772e3e206c
mohandodda0 <mohandodda@rocketmail.com>
2021-03-24 01:36:20 -0400

    Updates

---
+++ precache-manifest.ad462c5b505f840ca6289841889e4db6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15547af737136d33912703d1e50541b",
+    "url": "/eddie/index.html"
+  },
+  {
+    "revision": "b26890dbc2b3a8ba3879",
+    "url": "/eddie/static/css/main.aadd507c.chunk.css"
+  },
+  {
+    "revision": "c39151c74eba922c8cd9",
+    "url": "/eddie/static/js/2.dbb1b460.chunk.js"
+  },
+  {
+    "revision": "c7f92eccde8e496c47f87d08777731a5",
+    "url": "/eddie/static/js/2.dbb1b460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b26890dbc2b3a8ba3879",
+    "url": "/eddie/static/js/main.2cf626b8.chunk.js"
+  },
+  {
+    "revision": "c900707f9dc9d0b72157",
+    "url": "/eddie/static/js/runtime-main.b421db81.js"
+  }
+]);
